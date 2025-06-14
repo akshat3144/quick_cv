68af5d6136bbcb16c7ff8147c96d8c332b7d7097
Akshat Gupta <akshat3144@gmail.com>
2025-06-15 00:42:59 +0530

    Project Deployed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://quickcv-rrf2.onrender.com"
+        ? "https://quickcv-client.onrender.com"
         : "http://localhost:3000",
     credentials: true,
   })
