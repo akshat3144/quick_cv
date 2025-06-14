0d3292bebb99358583b3fef4f6dbc89af5e9699a
Akshat Gupta <akshat3144@gmail.com>
2025-06-15 00:09:28 +0530

    Deployment Fixes

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Set axios base URL based on environment
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "/api" // In production, we serve from the same domain
+    ? "https://quickcv-api.onrender.com/api" // In production, we serve from the same domain
     : "http://localhost:5000/api"; // In development, connect to separate backend server
 
 const api = axios.create({
